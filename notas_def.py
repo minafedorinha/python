bd607b7ff74de8bde797ffe3a14c871769e66d49
minafedorinha <137645096+minafedorinha@users.noreply.github.com>
2023-08-18 01:06:09 +0000

    notas colegial

---
+++ notas_def.py
@@ -0,0 +1,9 @@
+nota1 = float(input("digite sua  nota: "))
+nota2 = float(input("digite sua segunda nota: "))
+media = 0
+
+print("Sua primeira nota", nota1)
+print("Sua segunda nota" , nota2)
+
+
+
