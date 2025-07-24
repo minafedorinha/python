1699dae5be9cf9ce29036b9429ab1d65bec73445
Yasmin Victória <mina.dev@outlook.com>
2025-07-24 16:56:06 -0300

    Tirei informalidades, estou organizando meu Github

--- helloword.py
+++ helloword.py
@@ -1,1 +1,3 @@
 print("hello word")
+
+#primeiro hello word!
--- split.py
+++ split.py
@@ -3,6 +3,5 @@ eu = input("Digite os valores de A e B separados por espaço: ").split(" ")
 a = int(eu[0])
 b = float(eu[1])
 
-# Resto do código...
 # printar valores do Usuario.
 print("Valores de A e B:", a, b)
